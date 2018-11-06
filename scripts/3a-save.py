5fec246e0ad492cadbd8439d8487c86c7f77d9e6
root <root@localhost.localdomain>
2018-11-06 12:06:19 -0500

    update 06/11

--- scripts/3a-save.py
+++ scripts/3a-save.py
@@ -9,16 +9,23 @@ from shutil import make_archive
 import gzip
 import os
 import sys
-
+import datetime
 
 chemin_arr = os.path.expanduser('/root/data/')
 chemin_dep = os.path.expanduser('/root/B2-Python/scripts/')
 nom_archive = os.path.expanduser('/root/backup')
 
+
+#Création archive
 try:
-	os.path.exists(chemin_arr + '/backup.tar.gz')
 	make_archive(nom_archive, 'gztar', chemin_dep)
 	shutil.move(nom_archive + '.tar.gz', chemin_arr)
-except OSError:
-	sys.stderr.write('Le fichier existe déjà')
-	raise
+	sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
+except:
+	sys.stderr.write('Une sauvegarde du même nom existe déjà \n')
+	
+
+
+#except OSError:
+#	sys.stderr.write('Le fichier existe déjà')
+#	raise
