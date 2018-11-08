9b2a6b59f44ec50a00dc97bd7a31cbbc31a36c54
root <root@localhost.localdomain>
2018-11-08 05:27:06 -0500

    3a en cours[C

--- scripts/3a-save.py
+++ scripts/3a-save.py
@@ -16,14 +16,18 @@ chemin_dep = os.path.expanduser('/root/B2-Python/scripts/')
 nom_archive = os.path.expanduser('/root/backup')
 
 
-#Création archive
-try:
-	make_archive(nom_archive, 'gztar', chemin_dep)
-	shutil.move(nom_archive + '.tar.gz', chemin_arr)
-	sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
-except:
-	sys.stderr.write('Une sauvegarde du même nom existe déjà \n')
+#Verif des droits sur le fichier
+if os.access(chemin_arr, os.W_OK and os.R_OK):
+	#Création archive	
+	try:
+		make_archive(nom_archive, 'gztar', chemin_dep)	
+		shutil.move(nom_archive + '.tar.gz', chemin_arr)
+		sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
+	except:
+		sys.stderr.write('Une sauvegarde du même nom existe déjà \n')
 	
+else:
+	sys.stderr.write('Vous n\'avez pas les droits nécessaire')
 
 
 #except OSError:
