ee6f67808d91fbf6930bd7ee5c032b7006a18e44
root <root@localhost.localdomain>
2018-11-09 16:45:50 -0500

    3a-mol func update

--- scripts/3a-save.py
+++ scripts/3a-save.py
@@ -5,23 +5,41 @@
 # Import des modules
 import shutil
 from shutil import make_archive
+#import signal
 import gzip
 import os
 import sys
 import datetime
+
+#Fonction si CTRL + C
+#def quitProg(sig, frame):
+#        print('\nEt on quitte ça proprement svp!')
+#        exit()
+
+#signal.signal(signal.SIGINT, quitProg)
+
 
 chemin_arr = os.path.expanduser('/root/data/')
 chemin_dep = os.path.expanduser('/root/B2-Python/scripts/')
 nom_archive = os.path.expanduser('/root/backup')
 
 
+def createArchive():
+	make_archive(nom_archive, 'gztar', chemin_dep)
+	shutil.move(nom_archive + '.tar.gz', chemin_arr)
+	sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
+
+def deleteArchive():
+	os.remove('/root/data/backup.tar.gz')
+	os.remove('/root/backup.tar.gz')
+	sys.stdout.write('Votre ancienne sauvegarde a bien été supprimée.\n')
+
+
 #Verif du droit d'écriture sur le fichier
 if os.access(chemin_arr, os.W_OK):
 	#Création archive	
 	try:
-		make_archive(nom_archive, 'gztar', chemin_dep)	
-		shutil.move(nom_archive + '.tar.gz', chemin_arr)
-		sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
+		createArchive()
 	#Une sauvegarde du meme existe déjà
 	except:
 		sys.stderr.write('Une sauvegarde du même nom existe déjà \n')
@@ -29,15 +47,11 @@ if os.access(chemin_arr, os.W_OK):
 		#L'user veut remplacer l'ancienne
 		#Suppression de l'ancienne
 		if userDelete == 'yes':
-			os.remove('/root/data/backup.tar.gz')
-			os.remove('/root/backup.tar.gz')
-			sys.stdout.write('Votre ancienne sauvegarde a bien été supprimée.\n')
+			deleteArchive()
 			userCreate = input('Voulez vous créer une nouvelle sauvegarde? (yes/no) \n')
 			#Création de la nouvelle sauvegarde
 			if userCreate == 'yes':
-				make_archive(nom_archive, 'gztar', chemin_dep)
-				shutil.move(nom_archive + '.tar.gz', chemin_arr)
-				sys.stdout.write('Une archive a bien été crée. Elle se trouve dans' + chemin_arr + '. \n')
+				createArchive()
 			#L'ancienne est supprimée mais l'user n'en effectue pas de nouvelle. (chelou)
 			elif userCreate == 'no':
 				sys.stdout.write('Quel interêt de supprimer l\'ancienne dans ce cas...\n')
